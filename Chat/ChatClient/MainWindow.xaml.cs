93794ff4f012b673f2706cb0a19f9da21b7c1043
kormilitsynav <kormilicyn.av@gmail.com>
2024-08-18 22:58:54 +0300

    first

---
+++ Chat/ChatClient/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _clientSocket=new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            await _clientSocket.ConnectAsync("localhost", 33333);
+            _clientSocket.Bind(new IPEndPoint(IPAddress.Any, 33333));
+
+            Log.Text += $"Connected to {_clientSocket.RemoteEndPoint}\n";
+        }
+
+        private Socket _clientSocket;
+    }
+}
